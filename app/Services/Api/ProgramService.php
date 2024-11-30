87b0e7c3bee5be5af02e53316886c04e548222cf
deniskorbakov <korbakovd@gmail.com>
2024-11-30 18:19:22 +0300

    feat: create logic for get list solutions

--- app/Services/Api/ProgramService.php
+++ app/Services/Api/ProgramService.php
@@ -19,6 +19,7 @@ use App\Models\Solution;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class ProgramService
@@ -167,6 +168,29 @@ class ProgramService
         }
 
         return response()->json(['message' => 'задание не найдено'], 404);
+    }
+
+    public function getListSolutions(Program $program, int $lessonId, int $exerciseId, Request $request): JsonResponse|Response|array
+    {
+        $lesson = Lesson::query()->where('id', $lessonId)->where('program_id', $program?->id)->first();
+        $exercise = Exercise::query()->where('id', $exerciseId)->where('lesson_id', $lesson?->id)->first();
+
+        if (!$lesson && !$exercise) {
+            return response()->json(['message' => 'задание не найдено'], 404);
+        }
+
+        $solution = Solution::query()
+            ->where('student_id', auth()->id())
+            ->where('teacher_id', $program->users()->first()->id)
+            ->where('exercise_id', $exercise?->id);
+
+        if ($request->get('isVerified') === 'true') {
+            $solution->whereNotNull('mark');
+        } else if ($request->get('isVerified') === 'false') {
+            $solution->whereNull('mark');
+        }
+
+        return $solution->get()->toArray();
     }
 
     public function isSolved(Program $program, int $lessonId, int $exerciseId): array|JsonResponse
