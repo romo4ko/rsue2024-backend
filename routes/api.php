9b775a99fbc4966fff6cd699acc31b1db7d18925
deniskorbakov <korbakovd@gmail.com>
2024-11-30 16:49:08 +0300

    feat: create route

--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,7 @@ Route::group(['middleware' => ['auth:sanctum']], static function () {
         Route::get('/{id}/lessons/{lessonId}', [ProgramController::class, 'lesson'])->name('programs.lesson');
         Route::post('/{id}/lessons/{lessonId}/exercises', [ProgramController::class, 'storeExercises'])->name('programs.storeExercises');
         Route::delete('/{id}/lessons/{lessonId}/exercises/{exerciseId}', [ProgramController::class, 'removeExercises'])->name('programs.removeExercises');
+        Route::post('/{id}/lessons/{lessonId}/exercises/{exerciseId}/solutions/solve', [ProgramController::class, 'solutionsSolve'])->name('programs.solutionsSolve');
         Route::get('/{id}/lessons', [ProgramController::class, 'lessons'])->name('programs.lessons');
         Route::post('/{id}/lessons', [ProgramController::class, 'storeLesson'])->name('programs.storeLesson');
         Route::delete('/{id}/lessons/{lessonId}', [ProgramController::class, 'removeLesson'])->name('programs.removeLesson');
